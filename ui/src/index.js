573165d6c352d5e60e82d2ee2a23456f0fcaa86a
lbqds <lbqds@outlook.com>
2023-03-09 08:14:06 +0800

    Set testnet as default

--- ui/src/index.js
+++ ui/src/index.js
@@ -9,13 +9,14 @@ import { theme } from "./muiTheme";
 import { store } from "./state"
 import { Provider } from "react-redux"
 import { AlephiumConnectProvider } from "@alephium/web3-react"
+import { network, networkName } from "./utils/consts";
 
 ReactDOM.render(
   <ErrorBoundary>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-          <AlephiumConnectProvider chainGroup={0} network="testnet">
+          <AlephiumConnectProvider addressGroup={network.groupIndex} network={networkName}>
             <SnackbarProvider maxSnack={3}>
               <HashRouter>
                 <App />
