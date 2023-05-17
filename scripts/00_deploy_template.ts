8a820c697a2f353150d26e8e679565e82dd88505
lbqds <lbqds@outlook.com>
2023-05-17 17:04:52 +0800

    Fix deploy scripts

--- scripts/00_deploy_template.ts
+++ scripts/00_deploy_template.ts
@@ -3,6 +3,7 @@ import { TokenPair } from '../artifacts/ts'
 
 const deployTokenPairTemplate: DeployFunction<undefined> = async (deployer: Deployer): Promise<void> => {
   const initialFields = {
+    feeCollectorFactory: '',
     token0Id: '',
     token1Id: '',
     reserve0: 0n,
@@ -10,7 +11,9 @@ const deployTokenPairTemplate: DeployFunction<undefined> = async (deployer: Depl
     blockTimeStampLast: 0n,
     price0CumulativeLast: 0n,
     price1CumulativeLast: 0n,
-    totalSupply: 0n
+    totalSupply: 0n,
+    kLast: 0n,
+    feeCollectorId: ''
   }
   const result = await deployer.deployContract(TokenPair, { initialFields: initialFields })
   console.log(`TokenPair template contract address: ${result.contractInstance.address}, contract id: ${result.contractInstance.contractId}`)
