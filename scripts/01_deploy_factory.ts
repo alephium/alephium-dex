c035c4cf4e637fb4682c0a8652102a753676b131
lbqds <lbqds@outlook.com>
2023-05-18 19:46:17 +0800

    Check the group of feeSetter

--- scripts/01_deploy_factory.ts
+++ scripts/01_deploy_factory.ts
@@ -1,10 +1,14 @@
 import { Deployer, DeployFunction, Network } from '@alephium/cli'
+import { groupOfAddress } from '@alephium/web3'
 import { Settings } from '../alephium.config'
 import { TokenPairFactory } from '../artifacts/ts'
 
 const deployFactory: DeployFunction<Settings> = async (deployer: Deployer, network: Network<Settings>): Promise<void> => {
   if (network.settings.feeSetter === undefined) {
     throw new Error('Please specify the `feeSetter` in alephium.config.ts')
+  }
+  if (groupOfAddress(network.settings.feeSetter) !== deployer.account.group) {
+    throw new Error(`Invalid feeSetter, expected group ${deployer.account.group}`)
   }
   const tokenPairTemplate = deployer.getDeployContractResult('TokenPair')
   const initialFields = {
