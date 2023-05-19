b43889e0ca7008a2d9ca70d02fc88ed9684f6957
lbqds <lbqds@outlook.com>
2023-05-19 08:46:47 +0800

    Remove private key option from scripts

--- scripts/enable_fee_collector.ts
+++ scripts/enable_fee_collector.ts
@@ -1,13 +1,11 @@
-import { NetworkId, web3, networkIds, ONE_ALPH, DUST_AMOUNT } from "@alephium/web3"
-import { PrivateKeyWallet } from "@alephium/web3-wallet"
+import { NetworkId, web3, networkIds, SignExecuteScriptTxParams, addressFromPublicKey, TransactionBuilder, ONE_ALPH, DUST_AMOUNT } from "@alephium/web3"
 import { EnableFeeCollector } from "../artifacts/ts"
 import { loadDeployments } from "../artifacts/ts/deployments"
-import { waitTxConfirmed } from "./utils"
 import config from "../alephium.config"
 import { program, Option } from 'commander'
 
 async function enableFeeCollector(
-  feeSetterPrivateKey: string,
+  feeSetterPublicKey: string,
   networkId: NetworkId,
   tokenPairId: string
 ) {
@@ -15,32 +13,34 @@ async function enableFeeCollector(
   web3.setCurrentNodeProvider(network.nodeUrl)
   const deployments = loadDeployments(networkId)
   const tokenPairFactoryId = deployments.contracts.TokenPairFactory.contractInstance.contractId
-  const signer = new PrivateKeyWallet({ privateKey: feeSetterPrivateKey })
   const initialFields = {
     tokenPairFactory: tokenPairFactoryId,
     tokenPair: tokenPairId
   }
-  const result = await EnableFeeCollector.execute(signer, {
-    initialFields: initialFields,
+  const feeSetterAddress = addressFromPublicKey(feeSetterPublicKey)
+  const params: SignExecuteScriptTxParams = {
+    signerAddress: feeSetterAddress,
+    signerKeyType: 'default',
+    bytecode: EnableFeeCollector.script.buildByteCodeToDeploy(initialFields),
     attoAlphAmount: ONE_ALPH + DUST_AMOUNT
-  })
-  await waitTxConfirmed(web3.getCurrentNodeProvider(), result.txId, 1)
-  console.log(`EnableFeeCollector tx confirmed, tx id: ${result.txId}`)
+  }
+  const buildUnsignedTxResult = await TransactionBuilder.from(web3.getCurrentNodeProvider()).buildExecuteScriptTx(params, feeSetterPublicKey)
+  console.log(`EnableFeeCollector tx id: ${buildUnsignedTxResult.txId}, unsigned tx: ${buildUnsignedTxResult.unsignedTx}`)
 }
 
 program
-  .description('enable fee collector for token pair')
+  .description('build unsigned tx used to enable token pair fee collector')
   .addOption(new Option('--network-id <string>', 'network id').choices(networkIds).makeOptionMandatory())
-  .requiredOption('--fee-setter-private-key <string>', 'the private key of the feeSetter')
+  .requiredOption('--fee-setter-public-key <string>', 'the public key of the feeSetter')
   .requiredOption('--token-pair-id <string>', 'the contract id of the TokenPair')
   .action(async (opts) => {
     try {
-      const privateKey = opts.feeSetterPrivateKey as string
+      const publicKey = opts.feeSetterPublicKey as string
       const networkId = opts.networkId as NetworkId
       const tokenPairId = opts.tokenPairId as string
-      await enableFeeCollector(privateKey, networkId, tokenPairId)
+      await enableFeeCollector(publicKey, networkId, tokenPairId)
     } catch (error) {
-      console.log(`Failed to enable fee collector, error: ${error}`)
+      console.log(`Failed to build unsgined tx, error: ${error}`)
     }
   })
 
--- scripts/set_fee_collector_factory.ts
+++ scripts/set_fee_collector_factory.ts
@@ -1,13 +1,11 @@
-import { NetworkId, web3, networkIds } from "@alephium/web3"
-import { PrivateKeyWallet } from "@alephium/web3-wallet"
+import { NetworkId, web3, networkIds, addressFromPublicKey, SignExecuteScriptTxParams, TransactionBuilder } from "@alephium/web3"
 import { SetFeeCollectorFactory } from "../artifacts/ts"
 import { loadDeployments } from "../artifacts/ts/deployments"
-import { waitTxConfirmed } from "./utils"
 import config from "../alephium.config"
 import { program, Option } from 'commander'
 
 async function setFeeCollectorFactory(
-  feeSetterPrivateKey: string,
+  feeSetterPublicKey: string,
   networkId: NetworkId,
   feeCollectorFactoryId: string
 ) {
@@ -15,29 +13,33 @@ async function setFeeCollectorFactory(
   web3.setCurrentNodeProvider(network.nodeUrl)
   const deployments = loadDeployments(networkId)
   const tokenPairFactoryId = deployments.contracts.TokenPairFactory.contractInstance.contractId
-  const signer = new PrivateKeyWallet({ privateKey: feeSetterPrivateKey })
   const initialFields = {
     tokenPairFactory: tokenPairFactoryId,
     feeCollectorFactory: feeCollectorFactoryId
   }
-  const result = await SetFeeCollectorFactory.execute(signer, { initialFields: initialFields })
-  await waitTxConfirmed(web3.getCurrentNodeProvider(), result.txId, 1)
-  console.log(`SetFeeCollectorFactory tx confirmed, tx id: ${result.txId}`)
+  const feeSetterAddress = addressFromPublicKey(feeSetterPublicKey)
+  const params: SignExecuteScriptTxParams = {
+    signerAddress: feeSetterAddress,
+    signerKeyType: 'default',
+    bytecode: SetFeeCollectorFactory.script.buildByteCodeToDeploy(initialFields)
+  }
+  const buildUnsignedTxResult = await TransactionBuilder.from(web3.getCurrentNodeProvider()).buildExecuteScriptTx(params, feeSetterPublicKey)
+  console.log(`SetFeeCollectorFactory tx id: ${buildUnsignedTxResult.txId}, unsigned tx: ${buildUnsignedTxResult.unsignedTx}`)
 }
 
 program
-  .description('set fee collector factory')
+  .description('build unsigned tx used to set fee collector factory')
   .addOption(new Option('--network-id <string>', 'network id').choices(networkIds).makeOptionMandatory())
-  .requiredOption('--fee-setter-private-key <string>', 'the private key of the feeSetter')
+  .requiredOption('--fee-setter-public-key <string>', 'the public key of the feeSetter')
   .requiredOption('--fee-collector-factory-id <string>', 'the contract id of the FeeCollectorFactory')
   .action(async (opts) => {
     try {
-      const privateKey = opts.feeSetterPrivateKey as string
+      const publicKey = opts.feeSetterPublicKey as string
       const networkId = opts.networkId as NetworkId
       const feeCollectorFactoryId = opts.feeCollectorFactoryId as string
-      await setFeeCollectorFactory(privateKey, networkId, feeCollectorFactoryId)
+      await setFeeCollectorFactory(publicKey, networkId, feeCollectorFactoryId)
     } catch (error) {
-      console.log(`Failed to set FeeCollectorFactory, error: ${error}`)
+      console.log(`Failed to build unsigned tx, error: ${error}`)
     }
   })
 
